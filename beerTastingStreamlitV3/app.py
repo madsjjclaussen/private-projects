e12684508b8f22bc73e37b6db7a67062ace52b06
NovicellMadsC <mjc@novicell.dk>
2024-05-08 14:18:03 +0200

    Update app.py

--- beerTastingStreamlitV3/app.py
+++ beerTastingStreamlitV3/app.py
@@ -1,20 +1,22 @@
 import streamlit as st
-import configs
-df = configs.run_config(title="Dashboard")
-from rows import row1, row2, row3, row4, row5, row6, row7, row8
+# import configs
+# df = configs.run_config(title="Dashboard")
+# from rows import row1, row2, row3, row4, row5, row6, row7, row8
 
-row1.main(df)
-st.divider()
-row2.main(df)
-st.divider()
-row3.main(df)
-st.divider()
-row4.main(df)
-st.divider()
-row5.main(df)
-st.divider()
-row6.main(df)
-st.divider()
-row7.main(df)
-st.divider()
-row8.main(df)
+st.text("Hello World")
+
+# row1.main(df)
+# st.divider()
+# row2.main(df)
+# st.divider()
+# row3.main(df)
+# st.divider()
+# row4.main(df)
+# st.divider()
+# row5.main(df)
+# st.divider()
+# row6.main(df)
+# st.divider()
+# row7.main(df)
+# st.divider()
+# row8.main(df)
